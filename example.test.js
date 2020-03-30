b22d747c40f8189fda6f16804d7137992b9e2f2f
William Haw <me@williamhaw.com>
2020-03-30 19:49:17 +0800

    Add logging to example.test.js

--- example.test.js
+++ example.test.js
@@ -3,14 +3,19 @@ const fs = require("fs-extra");
 const assert = require("assert").strict;
 
 (async function () {
+  console.log("Starting first test case...");
   const responseOne = await got(`http://localhost:8080/one`);
   assert.strictEqual(responseOne.statusCode, 200);
   assert.strictEqual(responseOne.body, "one\n");
+  console.log("Test case succeeded!");
 
+  console.log("Starting second test case...");
   const responseTwo = await got(`http://localhost:8080/two`);
   assert.strictEqual(responseTwo.statusCode, 200);
   assert.strictEqual(responseTwo.body, '{"statuses":[1,2,3]}');
+  console.log("Test case succeeded!");
 
+  console.log("Starting third test case...");
   const responseThree = await got(`http://localhost:8080/three`);
   const expectedResponseString = fs
     .readFileSync("example-files-directory/example-file.json")
@@ -20,4 +25,5 @@ const assert = require("assert").strict;
     JSON.stringify(JSON.parse(responseThree.body)), //compare values
     JSON.stringify(JSON.parse(expectedResponseString))
   );
+  console.log("Test case succeeded!");
 })().catch(error => console.error(error));
